507b4ce21384350bdeb0a5a586c66ee81b8a5366
Rowland Momoh <momohrowland@gmail.com>
2025-07-25 03:11:29 +0100

    week 5: add Assignments program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Loius Litt", "Biology");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Mike Ross", "Algebra", "3.1", "5-10");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Harvey Specter", "American History", "The Declaration of Independence");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
