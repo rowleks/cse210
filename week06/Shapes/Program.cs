9ebca946e801d8cde3e437ee21bc63dff90cf85a
Rowland Momoh <momohrowland@gmail.com>
2025-08-03 18:46:10 +0100

    week 6: Add Shape program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = [new Square("Blue", 7), new Rectangle("Red", 4, 6), new Circle("Yellow", 5)];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
+
     }
 }
